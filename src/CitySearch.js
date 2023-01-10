ca7acc07db52c0a408b7da6c188d5a42c57c67ff
Vuyaa <st_im@hotmail.com>
2023-01-11 00:58:20 +0100

    using mockData

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,51 +1,53 @@
-// src/CitySearch.js
-
 import React, { Component } from 'react';
-
+// VARS and FUNCS //////////
+import { mockData } from './mock-data';
+import { extractLocations } from './api';
 
 class CitySearch extends Component {
-    state = {
-        query: '',
-        suggestions: []
-      }
+  state = {
+    query: "",
+    suggestions: [],
+  };
 
-handleInputChanged = (event) => {
-  const value = event.target.value;
-  const suggestions = this.props.locations.filter((location) => {
-    return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-  });
-  this.setState({
-    query: value,
-    suggestions,
-  });
-};
-
-handleItemClicked = (suggestion) => {
-    this.setState({
-      query: suggestion
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    const locations = extractLocations(mockData);
+    const suggestions = locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-  }
+    this.setState({
+      query: value,
+      suggestions,
+    });
+  };
 
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+    });
+  };
 
   render() {
     return (
       <div className="CitySearch">
-        <input 
-        type="text"
-        className="city"
-        value={this.state.query}
-        onChange={this.handleInputChanged}
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
         />
         <ul className="suggestions">
-            {this.state.suggestions.map((suggestion) => (
+          {this.state.suggestions.map((suggestion) => (
             <li
-                key={suggestion}
-                onClick={() => this.handleItemClicked(suggestion)}
-                >{suggestion}</li>
-            ))}
-            <li>
-                <b>See all cities</b>
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+            >
+              {suggestion}
             </li>
+          ))}
+          <li>
+            <b>See all cities</b>
+          </li>
         </ul>
       </div>
     );
